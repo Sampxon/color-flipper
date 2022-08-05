b660086d96de502ae27de66c1ee667b476b62a7d
anonxbt <108493672+anonxbt@users.noreply.github.com>
2022-08-05 20:45:16 +0100

    this is our first commit on color flipper

---
+++ setup/app.js
@@ -0,0 +1,11 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  const randomNum = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[randomNum];
+  console.log(colors[randomNum]);
+  color.textContent = `${colors[randomNum]}`;
+});
